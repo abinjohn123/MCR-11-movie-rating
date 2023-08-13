ef3ca88c88c64cecc06be6fec98a15538d6343f2
Abin John <abinjohnanto@gmail.com>
2023-08-13 11:25:11 +0530

    feat: header ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
 import './styles/App.scss';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes></Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="app-header d-flex-c-s gap24 p16">
+      <h1>IMDB</h1>
+      <input
+        type="text"
+        className="global-search p8"
+        placeholder="Search movies by title, cast and director"
+      />
+      <div className="d-flex gap8 app-nav">
+        <NavLink to="/">Movies</NavLink>
+        <NavLink to="/watch-list">Watch list</NavLink>
+        <NavLink to="/starred">Starred</NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
